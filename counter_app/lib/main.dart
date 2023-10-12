ea1a7d2699a963a591db4259dfbd614df5a6e975
Otavio Ferreira De Mesquita <otaviom.prestserv@vibraenergia.com.br>
2023-10-12 16:38:14 -0300

    Finalizando o contador.

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -4,19 +4,37 @@ import "package:flutter/material.dart";
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        width: 500,
-        height: 300,
-        color: Colors.amber,
-        child: Center(
-          child: Container(
-            width: 200,
-            height: 200,
-            color: Colors.red,
-          ),
-        ),
+      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Counter App - Clique e Add")),
+      body: Center(
+          child: Text(
+        "Contador: \n$count",
+        textAlign: TextAlign.center,
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
       ),
-    ),
-  ));
+    );
+  }
 }
